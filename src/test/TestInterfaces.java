94cbb284269d51650cd9b3cc986b23eb7d1f878c
Angel321 <anyomoal@gmail.com>
2022-09-07 11:56:19 -0500

    cambio uno

---
+++ src/test/TestInterfaces.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+
+import accesodatos.*;
+/**
+ *
+ * @author ANGELITA
+ */
+public class TestInterfaces {
+    public static void main(String[] args) {
+        IAccesoDatos datos = new ImplementacionMySql ();
+        //datos.listar();
+        imprimir(datos);
+    }
+    public static void imprimir(IAccesoDatos datos) {
+        datos.listar();
+    }
+}
